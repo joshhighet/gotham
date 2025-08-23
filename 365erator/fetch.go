399f2fe0c1835d6a0958b5e27ca007f07549a2ea
josh! <github@joshhighet.com>
2025-08-23 20:33:32 +1200

    Update fetch.go

--- 365erator/fetch.go
+++ 365erator/fetch.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -14,32 +14,53 @@ func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("provide a filename to work with!")
 	}
-	file, err := ioutil.ReadFile(os.Args[1])
+	file, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	domains := strings.Split(string(file), "\n")
+	var validDomains []string
+	for _, domain := range domains {
+		domain = strings.TrimSpace(domain)
+		if domain != "" {
+			validDomains = append(validDomains, domain)
+		}
+	}
 	f, err := os.Create(os.Args[1] + "365")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
 	var wg sync.WaitGroup
-	for _, domain := range domains {
+	var mu sync.Mutex
+	for _, domain := range validDomains {
 		wg.Add(1)
 		go func(domain string) {
 			defer wg.Done()
 			resp, err := http.Get("https://login.microsoftonline.com/" + domain + "/.well-known/openid-configuration")
 			if err != nil {
-				log.Fatal(err)
+				log.Printf("Error fetching %s: %v", domain, err)
+				return
 			}
-			body, err := ioutil.ReadAll(resp.Body)
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
 			if err != nil {
-				log.Fatal(err)
+				log.Printf("Error reading response for %s: %v", domain, err)
+				return
 			}
+
 			if resp.StatusCode == 200 {
-				tenantID := strings.Split(string(body), "sts.windows.net/")[1]
-				tenantID = strings.Split(tenantID, "/")[0]
-				fmt.Fprintln(f, domain + "," + tenantID)
+				bodyStr := string(body)
+				if strings.Contains(bodyStr, "sts.windows.net/") {
+					parts := strings.Split(bodyStr, "sts.windows.net/")
+					if len(parts) > 1 {
+						tenantID := strings.Split(parts[1], "/")[0]
+						mu.Lock()
+						fmt.Fprintln(f, domain+","+tenantID)
+						mu.Unlock()
+					}
+				}
 			}
 		}(domain)
 	}
